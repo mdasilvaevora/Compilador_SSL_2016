9ee5c17621e6f3eb83b74a52eb8978531b0a3bd7
ManuelGit <manudse10@gmai.com>
2016-09-17 20:35:20 -0300

    Bloque main, con validaciones

--- Scanner.c
+++ Scanner.c
@@ -5,6 +5,7 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 
 FILE * in;
@@ -15,7 +16,54 @@ typedef enum{
 
 char buffer[TAMLEX];
 
+TOKEN scanner();
 int columna(int c);
+int estadoFinal(int e);
+
+int main(int argc, char * argv[])
+{
+
+    TOKEN tok;
+    char nombreArchivo[TAMNOM];
+    int lenNombreArchivo;
+
+    //Validaciones
+
+if ( argc == 1 )
+{
+printf("Debe ingresar el nombre del archivo fuente en la linea de comandos\n");
+ return -1;
+}
+
+if ( argc != 2 )
+{
+printf("Numero incorrecto de argumentos\n");
+return -2;
+}
+    strcpy(nombreArchivo, argv[1]);
+    lenNombreArchivo = strlen(nombreArchivo);
+if ( lenNombreArchivo > TAMNOM )
+{
+printf("Nombre incorrecto del Archivo Fuente\n");
+return -3;
+}
+
+if ( nombreArchivo[lenNombreArchivo-1] != 'm' || nombreArchivo[lenNombreArchivo-2] != '.' )
+{
+printf("El nombre del Archivo Fuente debe contener una extension .m \n");
+return -4;
+}
+if ( (in = fopen(nombreArchivo, "r") ) == NULL )
+{
+printf("No se pudo abrir archivo fuente\n");
+return -5;
+}
+
+return 0;
+}
+
+
+
 int columna(int c){
 	if(isalpha(c)) return 0;
 	if(isdigit(c)) return 1;
@@ -31,18 +79,12 @@ int columna(int c){
 	if(isspace(c)) return 11;
 	return 12;
 }
-
-
-int estadoFinal(int e);
 int estadoFinal(int e){
 	if(e == 0 || e == 1 || e == 3 || e == 11 || e == 14) return 0;
 	return 1;
 }
-
-
-
 TOKEN scanner(){
-	static int tabla[NUMESTADOS][NUMCOLS] = 
+	static int tabla[NUMESTADOS][NUMCOLS] =
 			{{1,3,5,6,7,8,9,10,11,14,13,0,14},
 			 {1,1,2,2,2,2,2,2,2,2,2,2,2},
 			 {14,14,14,14,14,14,14,14,14,14,14,14,14},
@@ -57,10 +99,10 @@ TOKEN scanner(){
 			 {14,14,14,14,14,14,14,14,14,14,14,14,14},
 			 {14,14,14,14,14,14,14,14,14,14,14,14,14},
 			 {14,14,14,14,14,14,14,14,14,14,14,14,14}};
-		 
+
 	int car,col,estado,i;
 	i=estado=0;
-	
+
 	do{
 		car = fgetc(in);
 		col = columna(car);
@@ -72,7 +114,7 @@ TOKEN scanner(){
 	}
 	while(!(estadoFinal(estado)) && !(estado == 14));
 	buffer[i] = '\0';
-	
+
 	switch(estado){
 		case 2: if(col != 11){
 			ungetc(car,in);
@@ -96,5 +138,3 @@ TOKEN scanner(){
 	}
 	return 0;
 }
-
-int main(){return 0;}
