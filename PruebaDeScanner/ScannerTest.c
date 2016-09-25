830edebcd9ca12bfbb5616766e5ac6362aca0b67
ManuelGit <manudse10@gmai.com>
2016-09-25 15:36:32 -0300

    Seguir testeando

--- PruebaDeScanner/ScannerTest.c
+++ PruebaDeScanner/ScannerTest.c
@@ -69,8 +69,8 @@ int main(int argc, char * argv[])
 		printf("No se pudo abrir archivo fuente\n");
 		return -5;
 	}
-
-	scanner();
+	char c;
+	while(!feof(in))scanner();
 	return 0;
 }
 
@@ -104,24 +104,21 @@ TOKEN esReservada(){
 
 TOKEN scanner(){
 	static int tabla[NUMESTADOS][NUMCOLS] =
-            //L D + - ( ) , ; : = EOF sp OTRO
-			{
-			    {1,3,5,6,7,8,9,10,11,14,13,0,14},
-			    {1,1,2,2,2,2,2,2,2,2,2,2,2},
-/*ID*/	         	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-		     	    {4, 3,4,4,4,4,4,4,4,4,4,4,4},
-/*CTE*/          	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*+*/		     	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-                 	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*-*/			    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*(*/			    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*)*/			    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*,*/			    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*;*/			    {14,14,14,14,14,14,14,14,14,12,14,14,14},
-/*ASIG*/         	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*fdt*/		     	    {14,14,14,14,14,14,14,14,14,14,14,14,14},
-/*Err*/			    {14,14,14,14,14,14,14,14,14,14,14,14,14}
-            		};
+{   { 1,  3,  5,  6,  7,  8,  9, 10, 11, 14, 13,  0, 14 },
+    {  1,  1,  2,  2,  2,  2,2,  2,  2,  2,  2,  2,  2 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    {  4,  3,  4,  4,  4,  4,  4,  4,  4, 4, 4, 4, 4 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 12, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 },
+    { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 } };
 
 	int caracter,col,estado,i;
 	i=estado=0;
@@ -143,55 +140,21 @@ TOKEN scanner(){
 			ungetc(caracter,in);
 			buffer[i-1] = '\0';
 			}
-			if(esReservada()==ID && strlen(buffer)<32){ //Si es ID lo coloco en la TS
-			colocar(buffer,TS);
-			printf("Identificador");
-			}
-			return esReservada();
+			printf("\n Identificador = %s \n",buffer);
 		case 4: if(col != 11){
 			ungetc(caracter,in);
 			buffer[i-1] = '\0';
 			}
-			colocar(buffer,TS);//Lleva los digitos a la TS
-			printf("Constante");
-		case 5: printf("Suma");
-		case 6: printf("Resta");
-		case 7: printf("ParenIzq /n");
-		case 8: printf("ParenDer /n");
-		case 9: printf("Coma /n");
-		case 10: printf("PuntoYComa /n");
-		case 12: printf("Asignacion /n");
-		case 13: printf("Fin de Texto/n");
-		case 14: printf("Error Lexico /n");
+			printf("Constante = %s \n",buffer);
+		case 5: printf("Suma\n");
+		case 6: printf("Resta\n");
+		case 7: printf("ParenIzq \n");
+		case 8: printf("ParenDer \n");
+		case 9: printf("Coma \n");
+		case 10: printf("PuntoYComa \n");
+		case 12: printf("Asignacion \n");
+		case 13: printf("Fin de Texto\n");
+		case 14: printf("Error Lexico\n");
 	}
 	return 0;
-}
-
-int buscar(char * id, RegTS * TS, TOKEN * t)
-{
-    /* Determina si un identificador esta en la TS */
-    int i = 0;
-    while ( strcmp("$", TS[i].identificador) ) //Mientras no encuentre el centinela
-    {
-    if ( !strcmp(id, TS[i].identificador) ) //Cuando encuentra el identificador
-    {
-    *t = TS[i].tok;                           //Actualiza el Token a ID
-    return 1;
-    }
-    i++;
-    }
-    return 0;
-}
-
-void colocar(char * id, RegTS * TS)
-{
-    /* Agrega un ID a la TS */
-    int i = 4;
-    while ( strcmp("$", TS[i].identificador) ) i++; //Mientras no encuentre la centinela avanza en el array
-    if ( i < 999 )                                  // Controla que no se exceda del tamaño maximo del array
-    {
-    strcpy(TS[i].identificador, id );               //Actualiza el nombre del identificador y el token
-    TS[i].tok= ID;
-    strcpy(TS[++i].identificador, "$" );             //Avanza y coloca el centinela
-    }
 }
---
+++ PruebaDeScanner/progTest.m
@@ -0,0 +1,4 @@
+inicio leer (a,b); cc:= a + (b-2); escribir (cc,a+4); fin
+
+
+
